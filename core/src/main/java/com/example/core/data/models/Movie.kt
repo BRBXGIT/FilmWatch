dfd33db17fd66fd7b314fcc72f2c63d7a57b6ad8
BRBX <egorka2005sautkin@gmail.com>
2024-07-16 18:23:47 +0300

    start creating main screen

---
+++ core/src/main/java/com/example/core/data/models/Movie.kt
@@ -0,0 +1,6 @@
+package com.example.core.data.models
+
+data class Movie(
+    val adult: Boolean,
+    val id: Int
+)
---
+++ core/src/main/java/com/example/core/data/network/MainScreenApiInstance.kt
@@ -0,0 +1,15 @@
+package com.example.core.data.network
+
+import com.example.core.data.models.MoviesResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MainScreenApiInstance {
+
+    @GET("/movie/changes")
+    suspend fun getAllMovies(
+        @Query("page") page: Int,
+        @Query("api_key") apiKey: String
+    ): Response<MoviesResponse>
+}

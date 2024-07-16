acbd83b36fd6549cf4c68fe232e67028c4d75169
BRBX <egorka2005sautkin@gmail.com>
2024-07-16 16:23:47 +0300

    add colors to dark sheme

--- app/src/main/java/com/example/filmwatch/ui/theme/Color.kt
+++ app/src/main/java/com/example/filmwatch/ui/theme/Color.kt
@@ -2,9 +2,17 @@ package com.example.filmwatch.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Charcoal = Color(0xff111111)
+val RaisinBlack = Color(0xff242424)
+val MediumGray = Color(0xff747474)
+val DarkGray = Color(0xff8b8b8b)
+val CharcoalPurple = Color(0xff494358)
+val LavenderBlush = Color(0xffe8ddf8)
+val AlmostWhite = Color(0xffd1d1d1)
+val Gainsboro = Color(0xffd5d5d5)
+val DimGray = Color(0xff9a9a9a)
+val SlateGray = Color(0xff747474)
+val NeutralGray = Color(0xff9e9e9e)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)

b8cd0e2842540de13b5affeb90d06d866418f3b3
BRBX <egorka2005sautkin@gmail.com>
2024-07-22 02:30:02 +0300

    save commit paging not done yet fully, but it wirking

--- feature/src/main/java/com/example/feature/main_screens/main_screen/presentation/MainScreen.kt
+++ feature/src/main/java/com/example/feature/main_screens/main_screen/presentation/MainScreen.kt
@@ -1,40 +1,54 @@
 package com.example.feature.main_screens.main_screen.presentation
 
-import android.util.Log
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 
 @Composable
 fun MainScreen(
     innerPadding: PaddingValues,
-    mainScreenVM: MainScreenVM
+    mainScreenVM: MainScreenVM,
+    context: Context = LocalContext.current
 ) {
     val latestMovies = mainScreenVM.latestMovies.collectAsLazyPagingItems()
 
     LaunchedEffect(latestMovies.loadState) {
         if(latestMovies.loadState.refresh is LoadState.Error) {
-            Log.d("movies", (latestMovies.loadState.refresh as LoadState.Error).error.message.toString())
+            Toast.makeText(
+                context,
+                (latestMovies.loadState.refresh as LoadState.Error).error.message,
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
-    Log.d("movies", latestMovies.itemSnapshotList.items.toString())
-
-    LazyColumn(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(innerPadding)
     ) {
-        items(latestMovies.itemSnapshotList.items) { movie ->
-            Text(text = movie.id.toString())
+        if(latestMovies.loadState.refresh is LoadState.Loading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        } else {
+            LazyColumn {
+                items(latestMovies.itemSnapshotList.items) { movie ->
+                    Text(text = movie.id.toString())
+                }
+            }
         }
     }
 }

aec81ac31a2959c1c2da0b06f4edee52e78ac4f5
BRBX <egorka2005sautkin@gmail.com>
2024-07-23 19:10:53 +0300

    main screen latestMovies feature finally works fine, need to make code clearer

--- feature/src/main/java/com/example/feature/main_screens/main_screen/presentation/MainScreen.kt
+++ feature/src/main/java/com/example/feature/main_screens/main_screen/presentation/MainScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -56,10 +55,11 @@ fun MainScreen(
                     vertical = 16.dp
                 )
             ) {
-                items(latestMovies.itemSnapshotList.items, key = { it.id }) { movie ->
-                    MoviePreviewUi(
-                        moviePreview = movie
-                    )
+                items(latestMovies.itemCount, key = { index -> index }) { index ->
+                    val movie = latestMovies[index]
+                    movie?.let {
+                        MoviePreviewUi(moviePreview = it)
+                    }
                 }
 
                 item {
